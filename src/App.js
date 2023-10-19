37d4c12f9f564e5b79dbf34e5bff68dde205cfc6
Marian <mariansusanaas@gmail.com>
2023-10-18 21:06:20 -0400

    frontend started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Portada from './components/promo1'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='contenedor-general'>
+       <Portada
+        imgLetter='A'
+        mainText='El secreto del éxito empresarial: ¡Descubre el poder del logo!'
+        secText='Un identificador visual, conocido comúnmente como logotipo, es de vital importancia para tu negocio. No se trata simplemente de un dibujo o una imagen, sino de la representación visual de '
+        />
+        <Portada
+        imgLetter='B'
+        mainText='!El logo de Apple no siempre fue la mazana mordida !'
+        secText='El diseño gráfico es una disciplina esencial en el mundo empresarial, ya que juega un papel fundamental en la forma en que las marcas se comunican visualmente con su público objetivo. Una de las herramientas más poderosas dentro '
+        />
+     </div>
     </div>
   );
 }
