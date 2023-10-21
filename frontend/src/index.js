f534e3786869b985e50b673f12c78afa0b59f8bc
Marian <mariansusanaas@gmail.com>
2023-10-20 20:42:00 -0400

    backend merged

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,9 +8,6 @@ import WebsDone from './pages/WebsDone';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  /* </React.StrictMode> */
   <React.StrictMode>
     <WebsDone />
   </React.StrictMode>
