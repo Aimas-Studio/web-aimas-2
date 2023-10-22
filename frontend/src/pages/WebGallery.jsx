b454320ba7ceaab021aa1c862d6a946f169712bf
Marian <mariansusanaas@gmail.com>
2023-10-21 23:10:23 -0400

    merge conflicts resolved

--- frontend/src/pages/WebGallery.jsx
+++ frontend/src/pages/WebGallery.jsx
@@ -1,19 +1,46 @@
+import '../css/webs-done.css';
 import SocialDotsHeader from '../components/social-dots-header';
 import Pagina from '../components/promo2'
+import { useState, useEffect } from 'react';
+
 
 
 function WebGallery() {
-  return (
-    <div className="WebGalery">
-      <SocialDotsHeader/>
+  const [projects, setProject] = useState([])
 
-      <Pagina
-        dir='webs-developed/cchang'
-        mainText='CChang S.U.R.L'
-        secText='Página web institucional para empresa constructora CChang S.U.R.L. La misma contiene la descripción de la empresa, los servicios que ofrece, una galería de proyectos, su contacto y redes sociales.'
-				implementado='-IMPLEMENTADO-'
-				irSitio='-IR AL SITIO-'
-			/>
+  useEffect(()=> {
+      async function fetchProject(){
+        try {
+          const response = await fetch('http://127.0.0.1:8000/api/project/')
+          if (!response.ok){
+            throw new Error("La red no respondió de manera correcta")
+          }
+          const result = await response.json();
+          setProject(result)
+        }
+        catch(error){
+          console.error('Error consiguiendo los datos', error)
+        }}
+    fetchProject()
+    console.log(projects)
+    }, []);
+    
+  return (
+    <div className="WebsDone">
+      <SocialDotsHeader/>
+      {
+          projects.map((project) =>
+          <Pagina
+          dir={project.image}
+          mainText={project.title}
+          secText={project.body}
+          implementado={project.type}
+          irSitio={project.site}
+          url={project.url}  
+        />
+          )
+        }
+{/*       
 
 			<Pagina
         dir='webs-developed/elencanto'
@@ -109,7 +136,7 @@ function WebGallery() {
         secText='Rediseño de marca gráfica para plataforma de cursos online sobre trading y viajes creada por Terry Ijeoma, reconocida especialista del sector.'
 				implementado='-PROPUESTA-'
 				irSitio=''
-      />
+      /> */}
 
 
     </div>    
