2effc6c3d24a7d5d58efc350c51c2dadb45ffdcf
Marian <mariansusanaas@gmail.com>
2023-10-31 15:29:12 -0400

    puntos borrados

--- frontend/src/components/contratar-info-buttons.jsx
+++ frontend/src/components/contratar-info-buttons.jsx
@@ -8,14 +8,14 @@ function ContratarInfoButtons( {color1, color2, colorT1, colorT2, idLast, servic
   return (
     
         <div className='cont-info-but-container' id={idLast}>
-            <Link to={`https://wa.me/13057090744?text=Hola,%20me%20interesa%20contratar%20sus%20servicios%20de%20${servicioFormat}.`} 
+            <Link to={`https://wa.me/13057090744?text=Hola,%20me%20interesa%20contratar%20sus%20servicios%20de%20${servicioFormat}`} 
                 className='contratar-button cont-info-but' 
                 style = {{backgroundColor: color1, color: colorT1}}
             >
                 CONTRATAR SERVICIO
             </Link>
 
-            <Link to={`https://wa.me/13057090744?text=Hola,%20me%20gustaría%20conocer%20más%20acerca%20de%20sus%20servicios%20de%20${servicioFormat}.`} 
+            <Link to={`https://wa.me/13057090744?text=Hola,%20me%20gustaría%20conocer%20más%20acerca%20de%20sus%20servicios%20de%20${servicioFormat}`} 
                 className='solinfo-button cont-info-but' 
                 style = {{backgroundColor: color2, color: colorT2}}>
                 SOLICITAR INFO
